e37c0f3dcd130f97a9636c91c96f6efd210828bd
annatrajano <annatrajano95@gmail.com>
2022-10-22 18:14:43 -0300

    test: :test_tube: add req10 - test_counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,10 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    javascript = count_ocurrences("src/jobs.csv", "JavaScript")
+    python = count_ocurrences("src/jobs.csv", "Python")
+
+    assert javascript == 122
+    assert python == 1639
+    assert javascript != python
