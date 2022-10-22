8d34c606877e73563ef835f312a8409e591d8e1b
annatrajano <annatrajano95@gmail.com>
2022-10-22 17:34:10 -0300

    feat: :fire: add req09  -  create filter_by_salary_range

--- src/insights.py
+++ src/insights.py
@@ -60,18 +60,11 @@ def matches_salary_range(job, salary):
     
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    job_salary_list = []
+    for job in jobs:
+        try:
+            if isinstance(salary, int) and matches_salary_range(job, salary):
+                job_salary_list.append(job)
+        except ValueError:
+            pass
+    return job_salary_list
