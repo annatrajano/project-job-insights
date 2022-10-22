4a02a877810aad660526fbae491b85a2eaae65f7
annatrajano <annatrajano95@gmail.com>
2022-10-22 18:31:49 -0300

    test: :test_tube: add req11 - test_brazilian_jobs

--- src/counter.py
+++ src/counter.py
@@ -3,3 +3,4 @@ def count_ocurrences(path, word):
     read_data = file.read()
     word_count = read_data.lower().count(word.lower())
     return word_count
+
--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,7 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    path = 'tests/mocks/brazilians_jobs.csv'
+    result = {"title": "Maquinista", "salary": "2000", "type": "trainee"}
+    assert result in read_brazilian_file(path)
