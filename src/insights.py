6f663608c9b3bd66a144738290edf637f96834da
annatrajano <annatrajano95@gmail.com>
2022-10-22 15:54:28 -0300

    feat: :fire: add req07 - create filter_by_industries

--- src/insights.py
+++ src/insights.py
@@ -22,21 +22,11 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    job_industries_list = []
+    for job in jobs:
+        if job['industry'] == industry:
+            job_industries_list.append(job)
+    return job_industries_list
 
 
 def get_max_salary(path):
